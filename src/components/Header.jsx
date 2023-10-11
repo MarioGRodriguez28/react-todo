8af4c9a5cf14030a3ffbccee4ff6b61a88fccc44
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-10-11 21:00:30 +0200

    Recognition of dark mode according to OS, Done!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,32 @@ import MoonIcon from "./icons/MoonIcon";
 import SunIcon from "./icons/SunIcon";
 
 const Header = () => {
-    const [darkMode, setDarkMode] = useState(true);
-    useEffect(() => {
-        if(darkMode){
-          document.documentElement.classList.add("dark")
-        }else{
-          document.documentElement.classList.remove("dark")
-        }
-      
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("theme") === "dark");
 
-      
-    }, [darkMode]);
-    return (
-        <header className="container mx-auto px-4 pt-8">
-            <div className="flex justify-between">
-                <h1 className="uppercase text-xl font-bold tracking-[0.3em] text-white ">
-                    Todo
-                </h1>
-                <button onClick={() => setDarkMode(!darkMode)}>
-                    {darkMode ? <SunIcon /> : <MoonIcon />}
-                </button>
-            </div>
-        </header>
-    );
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <header className="container mx-auto px-4 pt-8">
+      <div className="flex justify-between">
+        <h1 className="uppercase text-xl font-bold tracking-[0.3em] text-white ">Todo</h1>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? <SunIcon /> : <MoonIcon />}
+        </button>
+      </div>
+    </header>
+  );
 };
+
 export default Header;
